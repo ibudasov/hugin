b44f40d92e3ef305bde8becf0fc62c28c497c035
Igor Budasov <igor.budasov@gmail.com>
2022-01-27 18:51:32 +0100

    detecting auto assigned port by heroku

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,8 @@ import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT || 3000);
+  const herokuAssignsRandomPortSoFirstWeCheckIfThePortIsAssigned = process.env.PORT || 3000;
+  await app.listen(herokuAssignsRandomPortSoFirstWeCheckIfThePortIsAssigned);
 }
 
 bootstrap();
